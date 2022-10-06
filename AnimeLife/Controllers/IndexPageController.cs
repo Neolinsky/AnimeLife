689bfd64c48701aea12ef82899a7658eddfe5e89
Neolinsky <andrewbesov2004@gmail.com>
2022-10-06 21:16:58 +0500

    added routing

---
+++ AnimeLife/Controllers/IndexPageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AnimeLife.Controllers;
+
+public class IndexPageController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
