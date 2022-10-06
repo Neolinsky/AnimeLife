47d724c687e1aec5c4cc08ee7617e4be1db0c3c7
Neolinsky <andrewbesov2004@gmail.com>
2022-10-06 21:06:39 +0500

    First commit

---
+++ AnimeLife/StartUp.cs
@@ -0,0 +1,15 @@
+﻿namespace AnimeLife.Extensions;
+
+public static class AppExtensions
+{
+    public static void RegisterServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddControllers();
+    }
+
+    public static void ConfigureApp(this WebApplication app)
+    {
+        app.UseHttpsRedirection();
+        app.MapControllers();
+    }
+}
